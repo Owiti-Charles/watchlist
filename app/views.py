92419354cc343bcb9e3e900bb9b08c911ed8641d
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-02 18:29:34 +0300

    a

---
+++ app/views.py
@@ -0,0 +1,17 @@
+from flask import render_template
+from . import app
+@app.route('/')
+def index():
+    '''
+    View root page function that returns the index page and its data
+    '''
+    message = 'Hello Flask'
+    return render_template('index.html',message = message)
+
+@app.route('/movie/<int:movie_id>')
+def movie(id):
+
+    '''
+    View movie page function that returns the movie details page and its data
+    '''
+    return render_template('movies.html',id = id)
