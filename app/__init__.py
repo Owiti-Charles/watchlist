b5f4f608a752dbb349d3d91025b5bd59fae304fc
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-06 16:36:11 +0300

    deployed

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,6 @@ from flask_bootstrap import Bootstrap
 
 app = Flask(__name__, instance_relative_config = True)
 app.config.from_object(DevConfig)
-app.config.from_pyfile("config.py")
 
 bootstrap = Bootstrap(app)
 
--- app/config.py
+++ app/config.py
@@ -1,10 +1,14 @@
+import os
+from dotenv import load_dotenv
+load_dotenv()
 class Config:
     """
     config parrent class
 
     """
     MOVIE_API_BASE_URL ='https://api.themoviedb.org/3/movie/{}?api_key={}'
-    SECRET_KEY = '<Flask WTF Secret Key>'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    MOVIE_API_KEY = os.environ.get('MOVIE_API_KEY')
 
 class ProdConfig(Config):
     """
