0e3d5a3131c80e844c75ae8a5e5e84b9174c9f38
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-11 16:35:27 +0300

    Add password authentication

--- app/models.py
+++ app/models.py
@@ -1,9 +1,10 @@
+from werkzeug.security import generate_password_hash,check_password_hash
 from . import db
 class Role(db.Model):
     __tablename__ = "roles"
     id = db.Column(db.Integer, primary_key = True)
     name = db.Column(db.String(255))
-    users = db.relationship('User', backref = 'role', lazy="dynamic")
+    users = db.relationship('User', backref = 'role', lazy="dynamic")    
     def __repr__(self):
         return f'User {self.name}'
 
@@ -12,7 +13,20 @@ class User(db.Model):
     __tablename__ = 'users'
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(255))
+    pass_secure = db.Column(db.String(255))
     role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+    
+    @property
+    def password(self):
+        raise AttributeError("You cannot read the pasword")
+
+    @password.setter
+    def password(self,password):
+        self.pass_secure = generate_password_hash(password)
+    
+    def verify_password(self, password):
+        return check_password_hash(self.pass_secure,password)
+    
     def __repr__(self):
         return f'User {self.username}'
 
