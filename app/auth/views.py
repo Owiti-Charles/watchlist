3daf5e54320c449253122cd6ffc883b6f7a53088
Owiti-Charles <mikeycharlesm7@gmail.com>
2019-09-12 07:59:34 +0300

    Add registration form to allow a user to regiater for the application

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,7 +1,20 @@
 from . import auth
-from flask import render_template
+from flask import render_template,redirect,url_for
+from .forms import RegForm
+from .. import db
+from ..models import User
 
 @auth.route('/login')
 def login():
     return render_template('auth/login.html')
-    
+
+@auth.route('/register',methods = ['POST','GET'])
+def register():
+    form = RegForm()
+    if form.validate_on_submit():
+        user = User(email = form.email.data, username = form.username.data, password = form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        return redirect(url_for('auth.login'))
+        title = 'Create New Account'
+    return render_template('auth/register.html',regform = form)
